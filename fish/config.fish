a6227b792ccb87ba87e09d5f37bda7ecf2c5b552
Jayakrishna <69712032+chjayakrishnajk@users.noreply.github.com>
2024-06-08 17:11:22 +0530

    First Commit

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+zoxide init fish | source
+alias ls="exa --icons --classify --colour=auto --sort=type --group-directories-first --header --modified --git --binary --long"
+alias la="ls -a"
+alias ll="ls -al"
+alias lt1="exa --long --tree --level=1"
+alias lt2="exa --long --tree --level=2"
+alias lt3="exa --long --tree --level=3"
+alias lt4="exa --long --tree --level=4"
+alias hx="helix"
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    zoxide init fish | source
+end
+
+thefuck --alias | source
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
