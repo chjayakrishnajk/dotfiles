2bc741261a0c2fb4aa03f65c902bb4d94a490065
Jayakrishna <69712032+chjayakrishnajk@users.noreply.github.com>
2024-07-05 22:57:53 +0530

    Made Flameshot to open upon pressing prt_scrn & Edited Fish config & Added more shortcuts in hx

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+set -gx PATH ~/.cargo/bin /usr/local/sbin /usr/local/bin /usr/bin /usr/lib/jvm/default/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
 zoxide init fish | source
 alias ls="exa --icons --classify --colour=auto --sort=type --group-directories-first --header --modified --git --binary --long"
 alias la="ls -a"
@@ -8,6 +9,9 @@ alias lt3="exa --long --tree --level=3"
 alias lt4="exa --long --tree --level=4"
 alias hx="helix"
 alias cd="z"
+alias clr="clear"
+alias bst="bash ~/startup.sh"
+alias gtop="radeontop"
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
